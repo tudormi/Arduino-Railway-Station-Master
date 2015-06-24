64636ab10b77af55abdef1e9c1beb042017bf28c
tudormi <mit.tudor@gmail.com>
2015-06-24 20:32:01 +0300

    Revert "before predare"
    This reverts commit 37f53d695bfceb4574ea266cd928445d81d33583.

--- src/main/webapp/resources/js/responsive.js
+++ src/main/webapp/resources/js/responsive.js
@@ -4,37 +4,22 @@
 $(document).ready(function(){
 
     var total = $('#station_scheme').width();
-    /* left side */
     $('#left_turnout_1').css('left',parseInt($('#track_1 .track').css('margin-left'),10)-122);
-    $('#turnout1_toggle').css('left',parseInt($('#left_turnout_2').css('left')) -40);
-    $('#turnout5_toggle').css('left',parseInt($('#left_turnout_2').css('left')) + $('#left_turnout_2').width() +10);
-    $('#turnout3_toggle').css('left',parseInt($('#right_turnout_4').css('left')) -47);
-
-    /* right side */
     $('#left_turnout_2').css('left',$('#track_5').width() + parseInt($('#track_5').css('left'),10) -122);
     $('#right_turnout_2').css('right',total - $('#track_2').width()- parseInt($('#track_2').css('left'),10) -122);
-    $('#turnout4_toggle').css('right',total - $('#track_2').width()- parseInt($('#track_2').css('left'),10) -189);
-
     $('#left_turnout_4').css('right',total - $('#track_4').width()- parseInt($('#track_46 .track').css('left'),10) -122);
-    $('#turnout6_toggle').css('right',total - $('#track_2').width()- parseInt($('#track_2').css('left'),10) -19);
-    $('#turnout2_toggle').css('right',total - $('#track_2').width()- parseInt($('#track_2').css('left'),10) - 234);
     $('#right_turnout_4').css('left', parseInt($('#track_4').css('left'),10) -122);
 
     $( window ).resize(function(){
         var total = $('#station_scheme').width();
-        /* left side */
         $('#left_turnout_1').css('left',parseInt($('#track_1 .track').css('margin-left'),10)-122);
-        $('#turnout1_toggle').css('left',parseInt($('#left_turnout_2').css('left')) - 40);
+        $('#turnout1_toggle').css('left',parseInt($('#left_turnout_2').css('left')) -40);
         $('#turnout5_toggle').css('left',parseInt($('#left_turnout_2').css('left')) + $('#left_turnout_2').width() +10);
-        $('#turnout3_toggle').css('left',parseInt($('#right_turnout_4').css('left')) -47);
-
-        /* right side */
         $('#left_turnout_2').css('left',$('#track_5').width() + parseInt($('#track_5').css('left'),10) -122);
         $('#right_turnout_2').css('right',total - $('#track_2').width()- parseInt($('#track_2').css('left'),10) -122);
         $('#left_turnout_4').css('right',total - $('#track_4').width()- parseInt($('#track_46 .track').css('left'),10) -122);
         $('#right_turnout_4').css('left', parseInt($('#track_4').css('left'),10) -122);
-
-
+        $('#turnout3_toggle').css('left',parseInt($('#right_turnout_4').css('left')) -47);
     });
 
 });
